630faf982539fc392b543e13d644c11b775d4716
tanmay <kuamrchaurasiatanmay@gmail.com>
2024-10-24 01:06:37 +0530

    12 done

---
+++ 12controlflow/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Enter your login count:")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	// Trim the newline character
+	input = strings.TrimSpace(input)
+
+	// Convert string input to an integer
+	logincount, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a valid number.")
+		return
+	}
+
+	var result string
+	if logincount < 10 {
+		result = "regular user"
+	} else if logincount >= 10 && logincount < 20 {
+		result = "premium user"
+	} else {
+		result = "admin"
+	}
+
+	fmt.Println(result)
+
+}
